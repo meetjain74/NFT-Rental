dfeaf5b80fbe6b6b72058d07338f1c2f1bba3376
rajatsonibkn2002 <rajatsonibkn2002@gmail.com>
2022-11-11 22:22:51 +0530

    added cards

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,62 @@
-import { Box } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
+import { useState } from "react";
+import "../../styles/dashboard.css"
+import RentCard from "../Card/RentCard";
+import LendCard from "../Card/LendCard";
 
 import Navbar from "../Navbar/Navbar";
 
 const Dashboard = () => {
+  const [type, setType] = useState<String>("rent");
+  const nftdata = require("../../constants/DefaultData.ts")['LEND_CARDS'];
+
   return (
     <>
       <Box sx={imageBackground}>
         <Navbar marginTop={1} />
       </Box>
+
+      <div className="dashboard-btn-div">
+        <Button className={type==="rent" ? 'active-type':'type-btn'} onClick = {()=>setType("rent")}>Rented</Button>
+        <Button className={type==="lend" ? 'active-type':'type-btn'} onClick = {()=>setType("lend")}>Lended</Button>
+      </div>
+
+      <Grid
+        container
+        sx={{
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {
+        nftdata.map((item:any, index:number)=>{        
+        return type==="rent"?
+          <RentCard nftName={item.name}
+            nftId={item.tokenId}
+            nftAddress={item.tokenAddress}
+            imageName={item.imageName}
+            nftPrice={item.nftPrice}
+            dailyRent={"0.01"}
+            collateral={"0.01"}
+            maximumDuration={"20"}
+            nftImageURL = {"featuredNFT1.png"}
+            standard={"ERC721"} />:
+          <LendCard nftName={"Liquid Hand 101"}
+            nftId={"123456789"}
+            nftAddress={"0x0000000000000000000000000000000000000000"}
+            imageName={"featuredNFT1.png"}
+            nftPrice={"2.36"}
+            dailyRent={"0.01"}
+            collateral={"0.01"}
+            maximumDuration={"20"}
+            nftImageURL = {"featuredNFT1.png"}
+            standard={"ERC721"} />})
+        }
+      </Grid>
+
+        
+      
     </>
   );
 };
